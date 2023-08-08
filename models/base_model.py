ec7abbaba960fa4954f73de25788ced04cfab669
DINAMOW <meemoo102039@gmail.com>
2023-08-08 08:21:12 +0000

    sec task

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     """this is the base model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """the init func"""
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+
 
     def __str__(self):
         """this string func"""
@@ -29,10 +36,24 @@ class BaseModel:
         return tmp
 
 
-if "__main__" == __name__:
-    usr1 = BaseModel()
-    print(usr1.to_dict())
-    print("***************")
-    test = usr1.to_dict()
-    for key in test.keys():
-        print("\t{}: ({}) - {}".format(key, type(test[key]), test[key]))
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
